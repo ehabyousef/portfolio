571f8d709db5fc730d10061ab62a3c6f0a82f45e
ehabyousef <ehabyousef022@gmail.com>
2023-10-18 03:54:37 +0300

    add data

--- src/Component/Main/Main.jsx
+++ src/Component/Main/Main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import "./Main.css"
+import { useState } from 'react'
 const Main = () => {
+    const [setActive, setsetActive] = useState('all');
     return (
         <div className='contain portfolio'>
             <div className="right_main">
-                <button className='active'>All Projects</button>
-                <button>Html & Css</button>
-                <button >Javascript</button>
-                <button>React</button>
-                <button>Team Work</button>
+                <button onClick={() => setsetActive('all')} className={setActive === "all" ? 'active' : null}>All Projects</button>
+                <button onClick={() => setsetActive('html')} className={setActive === "html" ? 'active' : null}>Html & Css</button>
+                <button onClick={() => setsetActive('js')} className={setActive === "js" ? 'active' : null}>Javascript</button>
+                <button onClick={() => setsetActive('react')} className={setActive === "react" ? 'active' : null}>React</button>
+                <button onClick={() => setsetActive('team')} className={setActive === "team" ? 'active' : null}>Team Work</button>
             </div>
             <div className="left_main">
                 <div className="card">
@@ -25,62 +27,7 @@ const Main = () => {
                         </div>
                     </div>
                 </div>
-                <div className="card">
-                    <img src="image.jpg" alt="" width='250px' />
-                    <div className="content" style={{ width: '250px' }}>
-                        <h1 className='tilte'>Portfolio</h1>
-                        <p className="subtitle">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda, nostrum.</p>
-                        <div className="icon">
-                            <div className="marks">
-                                <i className="fa-solid fa-link"></i>
-                                <i className="fa-brands fa-github"></i>
-                            </div>
-                            <a href="/">More <span><i className="fa-solid fa-arrow-right"></i></span></a>
-                        </div>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src="image.jpg" alt="" width='250px' />
-                    <div className="content" style={{ width: '250px' }}>
-                        <h1 className='tilte'>Portfolio</h1>
-                        <p className="subtitle">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda, nostrum.</p>
-                        <div className="icon">
-                            <div className="marks">
-                                <i className="fa-solid fa-link"></i>
-                                <i className="fa-brands fa-github"></i>
-                            </div>
-                            <a href="/">More <span><i className="fa-solid fa-arrow-right"></i></span></a>
-                        </div>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src="image.jpg" alt="" width='250px' />
-                    <div className="content" style={{ width: '250px' }}>
-                        <h1 className='tilte'>Portfolio</h1>
-                        <p className="subtitle">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda, nostrum.</p>
-                        <div className="icon">
-                            <div className="marks">
-                                <i className="fa-solid fa-link"></i>
-                                <i className="fa-brands fa-github"></i>
-                            </div>
-                            <a href="/">More <span><i className="fa-solid fa-arrow-right"></i></span></a>
-                        </div>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src="image.jpg" alt="" width='250px' />
-                    <div className="content" style={{ width: '250px' }}>
-                        <h1 className='tilte'>Portfolio</h1>
-                        <p className="subtitle">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda, nostrum.</p>
-                        <div className="icon">
-                            <div className="marks">
-                                <i className="fa-solid fa-link"></i>
-                                <i className="fa-brands fa-github"></i>
-                            </div>
-                            <a href="/">More <span><i className="fa-solid fa-arrow-right"></i></span></a>
-                        </div>
-                    </div>
-                </div>
+
             </div>
         </div>
     );
