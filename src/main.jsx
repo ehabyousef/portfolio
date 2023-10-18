a1ad8e86eea3782b75f20d91fcd25b081269ceda
ehabyousef <ehabyousef022@gmail.com>
2023-10-18 15:42:12 +0300

    edit light mode

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 
-import(
-  "https://fonts.googleapis.com/css2?family=Averia+Sans+Libre:ital,wght@0,300;0,400;0,700;1,300&family=Averia+Serif+Libre:wght@300;400;700&family=Cormorant+Garamond:wght@300;400;500;700&display=swap"
-);
-import(
-  "https://fonts.googleapis.com/css2?family=Averia+Sans+Libre:ital,wght@0,300;0,400;0,700;1,300&family=Averia+Serif+Libre:wght@300;400;700&family=Cormorant+Garamond:wght@300;400;500;700&family=Rubik:wght@300;400;600;700&display=swap"
-);
+import('https://fonts.googleapis.com/css2?family=Averia+Sans+Libre:ital,wght@0,300;0,400;0,700;1,300&family=Jura:wght@300;700&display=swap');
+import('https://fonts.googleapis.com/css2?family=Petrona:wght@300;400;600&display=swap');
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
