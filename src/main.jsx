1e290f78cf9cb360d303065dc99ffcaec5c19d8a
ehabyousef <ehabyousef022@gmail.com>
2023-10-18 14:33:37 +0300

    add fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import "@fortawesome/fontawesome-free/js/all.js"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import(
+  "https://fonts.googleapis.com/css2?family=Averia+Sans+Libre:ital,wght@0,300;0,400;0,700;1,300&family=Averia+Serif+Libre:wght@300;400;700&family=Cormorant+Garamond:wght@300;400;500;700&display=swap"
+);
+import(
+  "https://fonts.googleapis.com/css2?family=Averia+Sans+Libre:ital,wght@0,300;0,400;0,700;1,300&family=Cormorant+Garamond:wght@300;400;500;700&display=swap"
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
