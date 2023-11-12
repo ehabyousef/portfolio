297831825560522df91cde607a4361a6c2edab8d
‪Ehab Yousef‬‏ <74505597+ehabyousef@users.noreply.github.com>
2023-11-12 03:58:53 +0200

    add tricks

--- src/Component/Avatart/Avatar.jsx
+++ src/Component/Avatart/Avatar.jsx
@@ -1,34 +1,59 @@
 import React from "react";
 import "./Avatar.css";
 import Lottie from "lottie-react";
-import dark from '../../../public/laptop_dark.json';
-import { motion } from "framer-motion"
+import dark from "../../../public/laptop_dark.json";
+import { motion } from "framer-motion";
+import Typewriter from "typewriter-effect";
+
 const Avatar = () => {
     return (
         <div className="contain avatar">
             <div className="right">
-                <motion.img initial={{ opacity: 0 }}
+                <motion.img
+                    initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     transition={{ duration: 2 }}
-                    src="circle.png" alt="" width="130px" />
+                    src="circle.png"
+                    alt=""
+                    width="130px"
+                />
                 <div className="text">
                     <motion.h1
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         transition={{ duration: 2 }}
-                    >Software Developer, Designer and compotitive programmer</motion.h1>
+                    >
+                        <Typewriter
+                            options={{
+                                strings: [
+                                    "Software Developer",
+                                    "Designer",
+                                    "Competitive Programmer",
+                                ],
+                                autoStart: true,
+                                loop: true,
+                                deleteSpeed: 30,
+                                delay: 100,
+                            }}
+                        />
+                    </motion.h1>
                     <p>
                         I’m Ehab, a software designer and entrepreneur based in Cairo,
-                        Egypt. I’m the Developer and Designer who develop
-                        technologies that empower regular people to explore space on their
-                        own terms.
+                        Egypt. I’m the Developer and Designer who develop technologies that
+                        empower regular people to explore space on their own terms.
                     </p>
                 </div>
                 <div className="icons">
-                    <a href="https://www.facebook.com/profile.php?id=100080623690922" target="blank">
+                    <a
+                        href="https://www.facebook.com/profile.php?id=100080623690922"
+                        target="blank"
+                    >
                         <i className="fa-brands fa-facebook-f"></i>
                     </a>
-                    <a href="https://www.instagram.com/ehab.yousef.66/?utm_source=qr&igshid=MThlNWY1MzQwNA%3D%3D" target="blank">
+                    <a
+                        href="https://www.instagram.com/ehab.yousef.66/?utm_source=qr&igshid=MThlNWY1MzQwNA%3D%3D"
+                        target="blank"
+                    >
                         <i className="fa-brands fa-instagram"></i>
                     </a>
                     <a href="https://github.com/ehabyousef" target="blank">
@@ -40,8 +65,7 @@ const Avatar = () => {
                 </div>
             </div>
             <div className="left">
-
-                <Lottie style={{ height: '40rem' }} animationData={dark} />
+                <Lottie style={{ height: "40rem" }} animationData={dark} />
             </div>
         </div>
     );
